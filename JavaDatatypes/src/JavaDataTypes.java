a2d2999ff2f535dd2a4aca757f50d91c1d4bb681
AlehinDev <71827715+alehindev@users.noreply.github.com>
2021-09-30 12:17:09 +0300

    Java datatypes task from Hackerrank

---
+++ JavaDatatypes/src/JavaDataTypes.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class JavaDataTypes{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++){
+            String iStr = sc.next();
+            if (iStr > "9223372036854775807")
+        }
+    }
+}
